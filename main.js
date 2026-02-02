825180cc85f0126eef61e2c8769d60f684a248b9
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2026-02-02 16:25:22 -0300

    feat: adicionar propriedades de escolha em português

--- main.js
+++ main.js
@@ -8,7 +8,18 @@ const botaoTrocarCor = document.getElementById('botao-trocar-cor')
 function trocarCor() {
     // Pega a cor informada pelo usuário
     const corUsuario = document.getElementById('cor-usuario').value
-    document.documentElement.style.setProperty('--color-bg', corUsuario)
+
+    if(corUsuario == 'lima'){
+        document.documentElement.style.setProperty('--color-bg', 'lime')
+    } else if (corUsuario == 'bege'){
+        document.documentElement.style.setProperty('--color-bg', 'beige')
+    } else if (corUsuario == 'carmesim'){
+        document.documentElement.style.setProperty('--color-bg', 'crimson')
+    } else if (corUsuario == 'rosa-choque'){
+        document.documentElement.style.setProperty('--color-bg', 'hotpink')
+    } else {
+        document.documentElement.style.setProperty('--color-bg', corUsuario)
+    }
 }
 
 botaoTrocarCor.addEventListener('click', trocarCor)
