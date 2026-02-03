e45f43804661d4a177fbfc4c5f2ddbc9ce7f2948
GUSTAVO VIDAL DE ABREU <25203686@CFP127ALUNO.LOCAL>
2026-02-03 13:21:18 -0300

    fix: corrigir formatação dos botões

--- main.js
+++ main.js
@@ -1,41 +1,41 @@
 // Torna o JavaScript mais rígido com seu código
 'use strict'
 
-// Criação de variável
-const botaoTrocarCor = document.getElementById('botao-trocar-cor')
-const botaoVerde = document.getElementById('botao-verde')
-const botaoVermelho = document.getElementById('botao-vermelho')
+    // Criação de variável
+    const botaoTrocarCor = document.getElementById('botao-trocar-cor')
+    const botaoVerde = document.getElementById('botao-verde')
+    const botaoVermelho = document.getElementById('botao-vermelho')
 
-// Função para trocar a cor de fundo
-function trocarCor() {
-    // Pega a cor informada pelo usuário
-    const corUsuario = document.getElementById('cor-usuario').value
+    // Função para trocar a cor de fundo
+    function trocarCor() {
+        // Pega a cor informada pelo usuário
+        const corUsuario = document.getElementById('cor-usuario').value
 
-    // Troca de cor em português (lima, bege, carmesim, rosa-choque)
-    if (corUsuario == 'lima') {
-        document.documentElement.style.setProperty('--color-bg', 'lime')
-    } else if (corUsuario == 'bege') {
-        document.documentElement.style.setProperty('--color-bg', 'beige')
-    } else if (corUsuario == 'carmesim') {
-        document.documentElement.style.setProperty('--color-bg', 'crimson')
-    } else if (corUsuario == 'rosa-choque') {
-        document.documentElement.style.setProperty('--color-bg', 'hotpink')
-    } else {
-        document.documentElement.style.setProperty('--color-bg', corUsuario)
+        // Troca de cor em português (lima, bege, carmesim, rosa-choque)
+        if (corUsuario == 'lima') {
+            document.documentElement.style.setProperty('--color-bg', 'lime')
+        } else if (corUsuario == 'bege') {
+            document.documentElement.style.setProperty('--color-bg', 'beige')
+        } else if (corUsuario == 'carmesim') {
+            document.documentElement.style.setProperty('--color-bg', 'crimson')
+        } else if (corUsuario == 'rosa-choque') {
+            document.documentElement.style.setProperty('--color-bg', 'hotpink')
+        } else {
+            document.documentElement.style.setProperty('--color-bg', corUsuario)
+        }
     }
-}
 
-// Botão de cor verde
-function trocarCorVerde() {
-    document.documentElement.style.setProperty('--color-bg', 'green')
-}
+    // Botão de cor verde
+    function trocarCorVerde() {
+        document.documentElement.style.setProperty('--color-bg', 'green')
+    }
 
-// Botão de cor vermelho
-function trocarCorVermelho() {
-    document.documentElement.style.setProperty('--color-bg', 'red')
-}
+    // Botão de cor vermelho
+    function trocarCorVermelho() {
+        document.documentElement.style.setProperty('--color-bg', 'red')
+    }
 
-// Ativa somente com click
-botaoTrocarCor.addEventListener('click', trocarCor)
-botaoVerde.addEventListener('click', trocarCorVerde)
-botaoVermelho.addEventListener('click', trocarCorVermelho)
+    // Ativa somente com click
+    botaoTrocarCor.addEventListener('click', trocarCor)
+    botaoVerde.addEventListener('click', trocarCorVerde)
+    botaoVermelho.addEventListener('click', trocarCorVermelho)
